d4747ed0f9d7bf6fb0d336d4a09373449bfafb1a
sidharthrajwal <131239031+sidharthrajwal@users.noreply.github.com>
2023-04-19 13:25:01 +0530

    Create post_response.php

---
+++ post_response.php
@@ -0,0 +1,48 @@
+<?php 
+/*
+
+template name:stylingjson
+
+*/
+
+get_header();
+
+?>
+<style >#post-list {
+    list-style: none;
+    margin: 0;
+    padding: 0;
+}
+
+.post {
+    margin-bottom: 20px;
+}
+
+.title {
+    font-size: 24px;
+    margin-bottom: 10px;
+}
+
+.content {
+    font-size: 16px;
+    line-height: 1.5;
+    margin-bottom: 10px;
+}
+
+.permalink {
+    font-size: 14px;
+    color: #999;
+}
+</style>
+<ul style="margin-top:300px;" id="post-list">
+  
+    <!-- more posts... -->
+</ul>
+
+
+
+
+
+<?php 
+
+get_footer();?>
